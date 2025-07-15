43256938893cd31c1bd1afbf8b4245d8032d9179
Matt Keck <mkeck@metromont.com>
2025-07-15 15:44:08 -0400

    Update headers.js
    cleaned and better debugging

--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -1,7 +1,21 @@
 export default async (request, context) => {
   const response = await context.next();
-  response.headers.set("Content-Security-Policy", "frame-ancestors 'self' *.autodesk.com *.autodesk.eu *.aus.autodesk.com");
+  
+  // Set comprehensive CSP for ACC embedding
+  response.headers.set("Content-Security-Policy", 
+    "frame-ancestors 'self' *.autodesk.com *.autodesk.eu *.aus.autodesk.com *.autodeskbim360.com *.acc.autodesk.com; " +
+    "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: https://developer.api.autodesk.com https://*.netlify.app; " +
+    "connect-src 'self' https://developer.api.autodesk.com https://*.netlify.app"
+  );
+  
+  // Remove X-Frame-Options to allow framing
   response.headers.delete("X-Frame-Options");
+  
+  // Set CORS headers for API calls
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
   return response;
 };
 
